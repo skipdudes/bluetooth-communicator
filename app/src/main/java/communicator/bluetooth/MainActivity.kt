a6191a8a497368d95eee84c869c5a090b5e4b1a2
skipdudes <skipdudes.dev@gmail.com>
2026-01-11 19:14:45 +0100

    Add paired devices list

--- app/src/main/java/communicator/bluetooth/MainActivity.kt
+++ app/src/main/java/communicator/bluetooth/MainActivity.kt
@@ -2,27 +2,37 @@ package communicator.bluetooth
 
 import android.Manifest
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import java.util.UUID
 
 class MainActivity : AppCompatActivity() {
     private var bluetoothAdapter: BluetoothAdapter? = null
+    private var targetDevice: BluetoothDevice? = null
+
+    companion object {
+        val MY_UUID: UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
+        //val MY_UUID: UUID = UUID.fromString("8ce255c0-200a-11e0-ac64-0800200c9a66")
+    }
 
     private val requestEnableBluetooth = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
     ) { result ->
         if (result.resultCode == RESULT_OK) {
             Toast.makeText(this, "Bluetooth turned on", Toast.LENGTH_SHORT).show()
+            showPairedDevices()
         } else {
             Toast.makeText(this, "Cancelled turning on Bluetooth", Toast.LENGTH_SHORT).show()
         }
@@ -41,14 +51,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
         val bluetoothManager = getSystemService(BluetoothManager::class.java)
         bluetoothAdapter = bluetoothManager?.adapter
@@ -84,7 +87,56 @@ class MainActivity : AppCompatActivity() {
             val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
             requestEnableBluetooth.launch(enableBtIntent)
         } else {
-            Toast.makeText(this, "Bluetooth is already on", Toast.LENGTH_SHORT).show()
+            showPairedDevices()
+        }
+    }
+
+    private fun showPairedDevices() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED
+            && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+        ) {
+            return
+        }
+
+        val pairedDevices = bluetoothAdapter?.bondedDevices
+        val pairedDeviceList = ArrayList<String>()
+
+        if (!pairedDevices.isNullOrEmpty()) {
+            for (device in pairedDevices) {
+                pairedDeviceList.add("${device.name} [${device.address}]")
+            }
+        } else {
+            pairedDeviceList.add("No paired devices")
+        }
+
+        val pairedDevicesSpinner: Spinner = findViewById(R.id.device_spinner)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, pairedDeviceList)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        pairedDevicesSpinner.adapter = adapter
+
+        pairedDevicesSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                if (pairedDevices.isNullOrEmpty()) return
+
+                val deviceDesc = parent.getItemAtPosition(position).toString()
+                try {
+                    val mac = deviceDesc.substring(deviceDesc.indexOf('[') + 1, deviceDesc.indexOf(']'))
+
+                    for (device: BluetoothDevice in pairedDevices) {
+                        if (device.address == mac) {
+                            targetDevice = device
+                            break
+                        }
+                    }
+                } catch (e: Exception) {
+
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
         }
     }
 }
